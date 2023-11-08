433591e47e8e953579edec3db36655129a890779
libaowen <1139780848@qq.com>
2023-11-08 14:01:47 +0800

    框架搭建

---
+++ src/main/java/com/wzxxl/gamelogic/WzxxlGameLogicApplication.java
@@ -0,0 +1,13 @@
+package com.wzxxl.gamelogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WzxxlGameLogicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WzxxlGameLogicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wzxxl/gamelogic/WzxxlGameLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzxxl.gamelogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WzxxlGameLogicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
